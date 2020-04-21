5c6359c2a4cfffcd1187c7a92d9b5675bc7e7d8d
Kévin Painchaud <mail@kpcd.me>
2020-04-21 21:44:50 +0200

    :sparkles: Split player actions into a new component

--- components/Player/Player.vue
+++ components/Player/Player.vue
@@ -14,7 +14,7 @@
       @mousemove="handleOverlayMousemove"
       @click="handleOverlayClick"
     ></div>
-    <div class="player__video" @click="handleOverlayClick">
+    <div class="player__video-wrapper" @click="handleOverlayClick">
       <div v-show="loading" class="placeholder-player"></div>
       <youtube
         ref="youtube"
@@ -31,80 +31,22 @@
         </span>
       </div>
     </div>
-
-    <div class="player__actions" @mouseover="handleActionsMouseover">
-      <div>
-        <button class="player__btn-back-start" @click="backToStart">
-          <span>
-            {{ $t('player.backToStart') }}
-          </span>
-        </button>
-      </div>
-      <div>
-        <div>
-          <button @click="move(-300)">
-            <ArrowRightX3Icon class="horizontal-mirror" />
-            <span>-5 min</span>
-          </button>
-        </div>
-        <div>
-          <button @click="move(-60)">
-            <ArrowRightX2Icon class="horizontal-mirror" />
-            <span>-1 min</span>
-          </button>
-        </div>
-        <div>
-          <button @click="move(-15)">
-            <ArrowRightIcon class="horizontal-mirror" />
-            <span>-15 sec</span>
-          </button>
-        </div>
-        <div v-if="!isPlaying" class="player__btn-play">
-          <button @click="playVideo">
-            <PlayIcon />
-          </button>
-        </div>
-        <div v-else class="player__btn-pause">
-          <button @click="pauseVideo">
-            <PauseIcon />
-          </button>
-        </div>
-        <div>
-          <button @click="move(15)">
-            <ArrowRightIcon />
-            <span>+15 sec</span>
-          </button>
-        </div>
-        <div>
-          <button @click="move(60)">
-            <ArrowRightX2Icon />
-            <span>+1 min</span>
-          </button>
-        </div>
-        <div>
-          <button @click="move(300)">
-            <ArrowRightX3Icon />
-            <span>+5 min</span>
-          </button>
-        </div>
-      </div>
-      <div>
-        <div class="player__volume">
-          <button @click="handleVolumeMuteToggling">
-            <VolumeIconMute v-if="volume === 0" />
-            <VolumeIcon v-else />
-          </button>
-          <vue-slider
-            ref="volumeSlider"
-            v-model="volume"
-            @change="handleVolumeChange"
-          />
-        </div>
-        <button v-if="screenfullEnabled" @click="toggleFullscreen">
-          <FullscreenIcon />
-        </button>
-      </div>
-    </div>
+    <PlayerActions
+      v-show="!fullscreen || userIsActive"
+      ref="playerActions"
+      :is-playing="isPlaying"
+      :volume="volume"
+      :class="{
+        'white-bg': fullscreen
+      }"
+      @playing="playVideo"
+      @paused="pauseVideo"
+      @moved="move"
+      @backToStart="backToStart"
+      @volumeChanged="handleVolumeChange"
+      @fullscreenToggled="toggleFullscreen"
+      @mouseover.native="handlePlayerActionsMouseover"
+    />
   </div>
 </template>
 
@@ -113,64 +55,46 @@
 
 import { Vue, Component, Prop } from 'vue-property-decorator'
 import canAutoPlay from 'can-autoplay'
-import VueSlider from 'vue-slider-component'
-import 'vue-slider-component/theme/default.css'
 import screenfull from 'screenfull'
 import { IGame } from '~/types/game'
-import PlayIcon from '~/assets/images/icons/play.svg?inline'
-import PauseIcon from '~/assets/images/icons/pause.svg?inline'
-import ArrowRightIcon from '~/assets/images/icons/arrow-right.svg?inline'
-import ArrowRightX2Icon from '~/assets/images/icons/arrow-right-x2.svg?inline'
-import ArrowRightX3Icon from '~/assets/images/icons/arrow-right-x3.svg?inline'
-import VolumeIcon from '~/assets/images/icons/volume.svg?inline'
-import VolumeIconMute from '~/assets/images/icons/volume-mute.svg?inline'
-import FullscreenIcon from '~/assets/images/icons/fullscreen.svg?inline'
+import PlayerActions from '~/components/PlayerActions/PlayerActions.vue'
 
 @Component({
   components: {
-    VueSlider,
-    PlayIcon,
-    PauseIcon,
-    ArrowRightIcon,
-    ArrowRightX2Icon,
-    ArrowRightX3Icon,
-    VolumeIcon,
-    VolumeIconMute,
-    FullscreenIcon
+    PlayerActions
   }
 })
 export default class GamePage extends Vue {
   @Prop({ type: Object, required: true })
   private game!: IGame
 
+  public $refs!: {
+    player: any
+    playerActions: any
+    youtube: any
+  }
+
   private loading: boolean = true
   private fullscreen: boolean = false
   private userIsActive: boolean = false
   private overlayMousemoveTimeout!: number
   private touchscreen: boolean = 'ontouchstart' in document.documentElement
-  private screenfullEnabled: boolean = screenfull.isEnabled
+  private fullscreenEnabled: boolean = screenfull.isEnabled
   private isPlaying: boolean = false
   private volume: number = 100
   private currentTime: number = 0
   private currentTimeInterval!: number
-
-  public $refs!: {
-    player: any
-    volumeSlider: any
-    youtube: any
-  }
-
-  public async getPlayerState() {
-    return await this.player.getPlayerState()
-  }
-
   private playerVars: YT.PlayerVars = {
     controls: 0,
     rel: 0,
     playsinline: 1
   }
 
-  private get player(): YT.Player {
+  public async getPlayerState() {
+    return await this.youtubePlayer.getPlayerState()
+  }
+
+  private get youtubePlayer(): YT.Player {
     return this.$refs.youtube.player
   }
 
@@ -185,7 +109,7 @@ export default class GamePage extends Vue {
   private mounted() {
     // Set interval for the current time indicator
     this.currentTimeInterval = window.setInterval(async () => {
-      this.currentTime = await this.player.getCurrentTime()
+      this.currentTime = await this.youtubePlayer.getCurrentTime()
     }, 200)
 
     // Check if autoplay is allowed
@@ -196,29 +120,29 @@ export default class GamePage extends Vue {
       }
     })
 
-    // Store fullscreen state into a data
-    if (this.screenfullEnabled) {
-      screenfull.on('change', () => {
-        this.fullscreen = (screenfull as any).isFullscreen
-      })
+    // Store fullscreen state into data
+    if (this.fullscreenEnabled) {
+      ;(screenfull as any).on('change', this.handleScreenfullChange)
     }
 
-    // Manage player actions on fullscreen
-    document.addEventListener('keyup', this.toggleVideoOnKeyupOnFullscreen)
+    // Spacebar keyup
+    document.addEventListener('keyup', this.handleSpacebarKeyup)
+  }
+
+  private handleScreenfullChange(): void {
+    this.fullscreen = (screenfull as any).isFullscreen
   }
 
   /**
-   * Play/pause on space keyup on fullscreen
+   * Play/pause on spacebar keyup on fullscreen
    */
-  private toggleVideoOnKeyupOnFullscreen(event: any): void {
+  private handleSpacebarKeyup(event: any): void {
     if (
-      this.screenfullEnabled &&
+      this.fullscreenEnabled &&
       (screenfull as any).isFullscreen &&
       event.keyCode === 32 &&
-      // Check no button is focus
-      this.$refs.player &&
-      this.$refs.player.querySelectorAll('.player__actions button:focus')
-        .length === 0
+      // Check no player action button is focus
+      this.$refs.playerActions.querySelectorAll('button:focus').length === 0
     ) {
       this.toggleVideo()
     }
@@ -226,15 +150,8 @@ export default class GamePage extends Vue {
 
   private destroyed() {
     window.clearInterval(this.currentTimeInterval)
-    document.removeEventListener('keyup', this.toggleVideoOnKeyupOnFullscreen)
-  }
-
-  private playVideo() {
-    this.player.playVideo()
-  }
-
-  private pauseVideo() {
-    this.player.pauseVideo()
+    ;(screenfull as any).off('change', this.handleScreenfullChange)
+    document.removeEventListener('keyup', this.handleSpacebarKeyup)
   }
 
   private async toggleVideo() {
@@ -242,49 +159,44 @@ export default class GamePage extends Vue {
 
     // eslint-disable-next-line no-undef
     if (playerState === YT.PlayerState.PLAYING) {
-      this.player.pauseVideo()
+      this.youtubePlayer.pauseVideo()
     } else {
-      this.player.playVideo()
+      this.youtubePlayer.playVideo()
     }
   }
 
+  private playVideo() {
+    this.youtubePlayer.playVideo()
+  }
+
+  private pauseVideo() {
+    this.youtubePlayer.pauseVideo()
+  }
+
   private backToStart() {
-    this.player.seekTo(0, true)
+    this.youtubePlayer.seekTo(0, true)
   }
 
   private async move(offset: number) {
-    const currentTime = await this.player.getCurrentTime()
+    const currentTime = await this.youtubePlayer.getCurrentTime()
 
-    this.player.seekTo(currentTime + offset, true)
+    this.youtubePlayer.seekTo(currentTime + offset, true)
   }
 
-  private handleVolumeChange(): void {
-    this.player.setVolume(this.volume)
-  }
-
-  private handleVolumeMuteToggling(): void {
-    this.$refs.volumeSlider.setValue(this.volume > 0 ? 0 : 100)
+  private handleVolumeChange(volume: number): void {
+    this.youtubePlayer.setVolume(volume)
   }
 
   private toggleFullscreen(): void {
     if (screenfull.isEnabled) {
       if (!this.fullscreen) {
         screenfull.request(this.$refs.player)
-        this.userIsActive = false // Prevent actions to be shown
-        this.blurActionsButtons()
+        this.userIsActive = false // Prevent player actions to be shown
+        this.$refs.playerActions.blurButtons() // Blur all player action buttons
       } else {
         screenfull.exit()
       }
     }
-  }
-
-  /**
-   * Blur all actions buttons
-   */
-  private blurActionsButtons(): void {
-    this.$refs.player
-      .querySelectorAll('.player__actions button')
-      .forEach((button: any) => button.blur())
   }
 
   private handleOverlayMousemove(): void {
@@ -310,7 +222,11 @@ export default class GamePage extends Vue {
     }
   }
 
-  private handleActionsMouseover(): void {
+  /**
+   * Mouseover the player actions
+   */
+  private handlePlayerActionsMouseover(): void {
+    // Keep user as active
     if (!this.touchscreen) {
       if (this.overlayMousemoveTimeout) {
         clearTimeout(this.overlayMousemoveTimeout)
@@ -328,7 +244,7 @@ export default class GamePage extends Vue {
 .player {
   $self: &;
 
-  &__video {
+  &__video-wrapper {
     margin-bottom: 25px;
 
     .placeholder-player {
@@ -352,168 +268,6 @@ export default class GamePage extends Vue {
     }
   }
 
-  &__actions {
-    display: flex;
-
-    @include media-breakpoint-down(md) {
-      flex-wrap: wrap;
-    }
-
-    > div {
-      align-items: center;
-      display: flex;
-      justify-content: center;
-
-      @include media-breakpoint-down(xs) {
-        &:nth-child(2) {
-          flex-wrap: wrap;
-
-          > * {
-            display: flex;
-            width: 1/3 * 100%;
-            justify-content: center;
-
-            &#{$self}__btn-play,
-            &#{$self}__btn-pause {
-              margin: 5px 0;
-              width: 100%;
-            }
-          }
-        }
-      }
-
-      @include media-breakpoint-down(md) {
-        &:nth-child(1) {
-          order: 2;
-          width: 50%;
-        }
-
-        &:nth-child(2) {
-          border-bottom: 1px solid rgba(#fff, 0.15);
-          order: 1;
-          margin-bottom: 10px;
-          padding-bottom: 15px;
-          width: 100%;
-        }
-
-        &:nth-child(3) {
-          order: 3;
-          width: 50%;
-        }
-      }
-
-      @include media-breakpoint-up(md) {
-        > *:not(:last-child) {
-          margin-right: 5px;
-        }
-
-        &:nth-child(2) {
-          flex: 100%;
-        }
-      }
-
-      @include media-breakpoint-up(xl) {
-        > *:not(:last-child) {
-          margin-right: 15px;
-        }
-      }
-    }
-
-    button,
-    #{$self}__volume {
-      > svg {
-        transition: transform $transition-duration transform 0;
-      }
-
-      @media (hover: hover) {
-        &:hover {
-          > svg {
-            transform: scale(1.2);
-
-            &.horizontal-mirror {
-              transform: scale(1.2) rotate(-180deg);
-            }
-          }
-        }
-      }
-    }
-
-    button {
-      align-items: center;
-      background-color: transparent;
-      border: 0;
-      height: 56px;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      min-width: 56px;
-      padding: 0 6px;
-      user-select: none;
-      width: 56px;
-
-      > svg {
-        display: block;
-        min-width: 60%;
-        width: 60%;
-
-        path {
-          fill: #fff;
-        }
-      }
-
-      > span {
-        color: #fff;
-        font-size: 12px;
-      }
-
-      &#{$self}__btn-back-start {
-        > span {
-          font-size: 16px;
-          font-weight: 700;
-          line-height: 1.35em;
-        }
-      }
-    }
-
-    #{$self}__btn-play,
-    #{$self}__btn-pause {
-      button {
-        border: 3px solid #fff;
-        border-radius: 500px;
-      }
-    }
-
-    #{$self}__btn-play {
-      button > svg {
-        left: 2px;
-        position: relative;
-        top: 0;
-      }
-    }
-
-    #{$self}__volume {
-      align-items: center;
-      display: flex;
-
-      > svg {
-        cursor: pointer;
-        display: block;
-        height: 32px;
-        min-width: 32px;
-        width: 32px;
-
-        path {
-          fill: #fff;
-        }
-      }
-
-      .vue-slider {
-        flex: 1;
-        min-width: 50px;
-      }
-    }
-  }
-
   &.fullscreen {
     #{$self}__overlay {
       background-color: $gray-900;
@@ -534,69 +288,12 @@ export default class GamePage extends Vue {
       z-index: 2;
     }
 
-    #{$self}__actions {
-      background-color: #fff;
-      bottom: 35px;
-      border-radius: 32px;
-      box-shadow: 0 0 20px 0 rgba(#000, 0.6);
-      display: none;
-      left: 50%;
-      max-width: 325px;
-      padding: 15px;
-      position: absolute;
-      transform: translateX(-50%);
-      width: calc(100% - 30px);
-      z-index: 3;
-
-      @include media-breakpoint-up(sm) {
-        max-width: 1000px;
-        padding: 15px 25px;
-      }
-
-      @include media-breakpoint-up(lg) {
-        bottom: 100px;
-      }
-
-      > div {
-        @include media-breakpoint-down(md) {
-          &:nth-child(2) {
-            border-bottom-color: $gray-900;
-          }
-        }
-      }
-
-      button {
-        > svg path {
-          fill: $gray-900;
-        }
-
-        > span {
-          color: $gray-900;
-        }
-      }
-
-      #{$self}__btn-play,
-      #{$self}__btn-pause {
-        button {
-          border-color: $gray-900;
-        }
-      }
-    }
-
     &.user-is-active {
       #{$self}__overlay {
         opacity: 0.8;
       }
-
-      #{$self}__actions {
-        display: flex;
-      }
     }
   }
-}
-
-.horizontal-mirror {
-  transform: rotate(-180deg);
 }
 </style>
 
@@ -611,30 +308,10 @@ export default class GamePage extends Vue {
     width: 100%;
   }
 
-  .vue-slider {
-    margin-right: 7px;
-  }
-
-  .vue-slider-process {
-    background-color: $primary;
-  }
-
-  .vue-slider-dot-tooltip-inner {
-    background-color: #fff;
-    border: 0;
-    border-color: #fff;
-    color: $gray-900;
-    line-height: 1em;
-    padding-top: 3px;
-  }
-
-  .vue-slider-dot-tooltip-inner-top::after {
-    border-width: 7px;
-    top: calc(100% - 1px);
-  }
-
-  &.loading iframe.youtube-player {
-    height: 0;
+  &.loading {
+    iframe.youtube-player {
+      height: 0;
+    }
   }
 
   &.fullscreen {
@@ -646,10 +323,23 @@ export default class GamePage extends Vue {
       width: 100%;
     }
 
-    .vue-slider-dot-tooltip-inner {
-      background-color: $gray-900;
-      border-color: $gray-900;
-      color: #fff;
+    .player-actions {
+      bottom: 35px;
+      left: 50%;
+      max-width: 325px;
+      position: absolute;
+      transform: translateX(-50%);
+      width: calc(100% - 30px);
+      z-index: 3;
+
+      @include media-breakpoint-up(sm) {
+        max-width: 1000px;
+        padding: 15px 25px;
+      }
+
+      @include media-breakpoint-up(lg) {
+        bottom: 100px;
+      }
     }
   }
 }
